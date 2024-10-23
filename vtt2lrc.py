ae28c9e429a21f6cb84bf88017568cb6e62dfbaa
limshixun <limshixun1106@gmail.com>
2024-10-23 23:12:53 +0800

    v0.1

--- vtt2lrc.py
+++ vtt2lrc.py
@@ -6,7 +6,6 @@ def rmNextLine(list):
     for idx, elem in enumerate(list):
         if '\n' in elem:
             new_elem = str(elem).replace('\n','')
-            print(list[-1])
         else:
             new_elem = elem
         list[idx] = new_elem
@@ -26,27 +25,44 @@ def reformatTime(lines):
     lines = [ re.sub(regex,'',x) for x in lines ]
     lines = [ x.strip() for x in lines ]
     
-    for idx, elem in enumerate(lines):
-        
-
-        
+    # for idx, elem in enumerate(lines):
+    hms_regex = r'(\d+:){2}\d+.\d+'
+    lines = [changeTimeFormat(x) if re.search(hms_regex,x) else x for x in lines ]
 
     return lines
 
 def changeTimeFormat(time):
     h, m, s = str(time).split(':')
-    m += int(m) + 60 * int(h)
+    m = int(m) + 60 * int(h)
+
+    MM = f'0{m}' if len(str(m)) == 1 else m
+    # zfill amke sures that the result has at least 5 char, else pad with zeros
+    SS = "{:.2f}".format(float(s)).zfill(5)
+
+    return f'[{MM}:{SS}]'
 
 def vtt2lrc(file):
     with open(file,'r',encoding='utf-8') as f:
         lines = f.readlines()
-        lines = rmNextLine(lines)
-        lines = rmNum(lines)
-        lines = list(filter(lambda x:x != '', lines))  
-        lines = list(filter(lambda x:x != 'WEBVTT', lines))
-        lines = list(filter(lambda x:x != 'Telegram@djyqlfy', lines))
-        lines = reformatTime(lines)
-        print(lines)
+
+    lines = rmNextLine(lines)
+    lines = rmNum(lines)
+    lines = list(filter(lambda x:x != '', lines))  
+    lines = list(filter(lambda x:x != 'WEBVTT', lines))
+    lines = list(filter(lambda x:x != 'Telegram@djyqlfy', lines))
+    lines = reformatTime(lines)
+    final_lines = []
+
+    for idx, itm in enumerate(lines):
+        if (idx) % 2 == 0:
+            final_lines.append(f'{itm}{lines[idx+1]}')
+
+    final_lines = '\n'.join(final_lines)
+    output_name, extension = os.path.splitext(file)
+    with open(f'{output_name}.lrc','a',encoding='utf-8') as f:
+        f.writelines(final_lines)
+
+    print(final_lines)
 
 if __name__ == '__main__':
     # if len(sys.argv != 2):
@@ -55,5 +71,3 @@ if __name__ == '__main__':
     for file in os.listdir():
         if file.endswith('.vtt'):
             vtt2lrc(file)
-    # for file in os.listdir():
-    #     vtt2lrc(file)
