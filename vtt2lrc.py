0f995ae299c2885762b40d20d411a489a2f94710
limshixun <limshixun1106@gmail.com>
2024-10-24 13:06:28 +0800

    fix lyric with \n in between to break the conversion format

--- vtt2lrc.py
+++ vtt2lrc.py
@@ -1,73 +1,79 @@
 import os
-import sys
 import re
 
-def rmNextLine(list):
-    for idx, elem in enumerate(list):
+def rmNextLine(lines):
+    for idx, elem in enumerate(lines):
         if '\n' in elem:
             new_elem = str(elem).replace('\n','')
         else:
             new_elem = elem
-        list[idx] = new_elem
-    return list
+        lines[idx] = new_elem
+    return lines
 
-def rmNum(list):
-    for idx, elem in enumerate(list):
-        # Match all char with only numbers
+def rmNum(lines):
+    for idx, elem in enumerate(lines):
         regex = r'^\d+$'
         new_elem = re.sub(regex,'',elem)
-        list[idx] = new_elem
-    return list
+        lines[idx] = new_elem
+    return lines
+
+def isNum(line):
+    regex = r'^\d+$'
+    if re.match(regex,line):
+        return True
+    return False
 
 def reformatTime(lines):
     # Match all --> a....
     regex = r'-->.+'
     lines = [ re.sub(regex,'',x) for x in lines ]
     lines = [ x.strip() for x in lines ]
-    
     # for idx, elem in enumerate(lines):
     hms_regex = r'(\d+:){2}\d+.\d+'
     lines = [changeTimeFormat(x) if re.search(hms_regex,x) else x for x in lines ]
-
     return lines
 
 def changeTimeFormat(time):
     h, m, s = str(time).split(':')
     m = int(m) + 60 * int(h)
-
     MM = f'0{m}' if len(str(m)) == 1 else m
     # zfill amke sures that the result has at least 5 char, else pad with zeros
     SS = "{:.2f}".format(float(s)).zfill(5)
-
     return f'[{MM}:{SS}]'
 
 def vtt2lrc(file):
-    with open(file,'r',encoding='utf-8') as f:
+    with open(file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     lines = rmNextLine(lines)
-    lines = rmNum(lines)
-    lines = list(filter(lambda x:x != '', lines))  
-    lines = list(filter(lambda x:x != 'WEBVTT', lines))
-    lines = list(filter(lambda x:x != 'Telegram@djyqlfy', lines))
+
+    lines = list(filter(lambda x: x != '', lines))  
+    lines = list(filter(lambda x: x != 'WEBVTT', lines))
+    lines = list(filter(lambda x: x != 'Telegram@djyqlfy', lines))
     lines = reformatTime(lines)
+
     final_lines = []
 
     for idx, itm in enumerate(lines):
-        if (idx) % 2 == 0:
-            final_lines.append(f'{itm}{lines[idx+1]}')
-
+        if idx + 1 < len(lines):
+            if isNum(itm):
+                final_lines.append(lines[idx+1])
+            elif not(isNum(itm)) and not(isNum(lines[idx + 1])):
+                final_lines[-1] += lines[idx+1]
     final_lines = '\n'.join(final_lines)
-    output_name, extension = os.path.splitext(file)
-    with open(f'{output_name}.lrc','a',encoding='utf-8') as f:
-        f.writelines(final_lines)
-
+        
     print(final_lines)
+    output_name = ''
+    if '.wav.vtt' in file:
+        output_name = file.replace('.wav.vtt','')
+    else:
+        output_name, _ = os.path.splitext(file)
+
+    with open(f'{output_name}.lrc', 'a', encoding='utf-8') as f:
+        f.write(final_lines + '\n')
 
 if __name__ == '__main__':
-    # if len(sys.argv != 2):
-    #     Exception('Usage: vtt2lrc <> <>')
-    
     for file in os.listdir():
         if file.endswith('.vtt'):
             vtt2lrc(file)
+    trashed_files = ['a','b']
